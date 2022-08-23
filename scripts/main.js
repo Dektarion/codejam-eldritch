46b1c8c164e1c1ed28b1de247562e6bf9ab5ff9c
Dektarion <andreiy.boyarsky@gmail.com>
2022-08-24 02:13:35 +0300

    feat: path of normal difficulty script

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 import ancientsData from '../data/ancients.js';
 import difficulties from '../data/difficulties.js';
-import blueCardsData from '../data/mythicCards/blue/index.js'
-import brownCardsData from '../data/mythicCards/brown/index.js'
-import greenCardsData from '../data/mythicCards/green/index.js'
+import blueCardsData from '../data/mythicCards/blue/index.js';
+import brownCardsData from '../data/mythicCards/brown/index.js';
+import greenCardsData from '../data/mythicCards/green/index.js';
 
 // console.log(greenCardsData[0].cardFace);
 // console.log(ancientsData[0].allCardsCount);
@@ -16,12 +16,19 @@ const difficultyButtons = document.querySelectorAll('.difficulty__buttons');
 const deckButtonDisplay = document.querySelector('.deck-button__display');
 const deckContainer = document.querySelector('.deck__container');
 const deckButton = document.querySelector('.deck-button');
+const cardPlace = document.querySelector('.card');
+const deskBackground = document.querySelector('.deck');
+
+/* Информация для циклов */
 
 let gameInformation = {
   idGod: '',
   idDifficulty: '',
   numInArr: 0,
-}
+};
+
+let allStageArrObj = {};
+
 
 const addNumGod = () => {
   switch(gameInformation.idGod) {
@@ -38,7 +45,9 @@ const addNumGod = () => {
       gameInformation.numInArr = 3;
       break;
   }
-}
+};
+
+/* Выбор древнего */
 
 const activeAncient = (event) => {
   for (let element of ancients) {
@@ -50,9 +59,11 @@ const activeAncient = (event) => {
     event.target.classList.add('active_ancient');
     difficultyButtonsContainer.classList.remove('difficulty__display_hidden');
     deckContainer.classList.add('hidden');
+    
   }
-  
 };
+
+/* Выбор сложности */
 
 const activeDifficultyButtons = (event) => {
   for (let element of difficultyButtons) {
@@ -69,9 +80,11 @@ const activeDifficultyButtons = (event) => {
 ancientsContainer.addEventListener('click', activeAncient);
 difficultyButtonsContainer.addEventListener('click', activeDifficultyButtons);
 
+/* Выбор нужного количества карт каждого цвета */
+
 const getGreenCardsImgArr = () => {
-  const min = Math.ceil(0);
-  const max = Math.floor(17);
+  const min = 0;
+  const max = 17;
   let randomGreenImgArr = [];
   let check;
 
@@ -86,11 +99,11 @@ const getGreenCardsImgArr = () => {
     while(check);
   }
   return randomGreenImgArr;
-}
+};
 
 const getBrownCardsImgArr = () => {
-  const min = Math.ceil(0);
-  const max = Math.floor(20);
+  const min = 0;
+  const max = 20;
   let randomBrownImgArr = [];
   let check;
 
@@ -105,11 +118,11 @@ const getBrownCardsImgArr = () => {
     while(check);
   }
   return randomBrownImgArr;
-}
+};
 
 const getBlueCardsImgArr = () => {
-  const min = Math.ceil(0);
-  const max = Math.floor(11);
+  const min = 0;
+  const max = 11;
   let randomBlueImgArr = [];
   let check;
 
@@ -124,7 +137,9 @@ const getBlueCardsImgArr = () => {
     while(check);
   }
   return randomBlueImgArr;
-}
+};
+
+/* Создание массива массивов отобранных карт*/
 
 const getImgArr = () => {
   let allChosenCardsArr = [];
@@ -134,19 +149,106 @@ const getImgArr = () => {
   allChosenCardsArr.push(getBlueCardsImgArr());
 
   return allChosenCardsArr;
+};
+
+/* Функция перемешивания карт внутри массива */
+
+function shuffleArray(arr) {
+  let shuffleArr = [];
+  let lenght = arr.length;
+  while (lenght > 0) {
+    let index = Math.floor(Math.random() * lenght);
+    shuffleArr.push(arr[index]);
+    arr.splice(index, 1);
+    lenght--;
+  }
+  return shuffleArr;
 }
 
+/* Отбор карт для каждого этапа */
 
-const getCardsForFirstStage = () => {
+// var allStageArrGlobal;
+
+let arrAllValues = [];
+
+const flattenArr = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const currentValue = arr[i];
+    if (Array.isArray(currentValue)) {
+      flattenArr(currentValue);
+    } else {
+      arrAllValues.push(currentValue);
+    }
+  }
+  return arrAllValues;
+};
+
+
+const getCardsForAllStage = () => {
+  let allStageArr = [];
   let firstStageArr = [];
+  let secondStageArr = [];
+  let thirdStageArr = [];
   let allChosenCardsArr = getImgArr();
 
-
-  for (let i = 0; i < ancientsData[gameInformation.numInArr].firstStage.greenCards; i++) {
-    firstStageArr.push(allChosenCardsArr[0][0].pop);
+  for (let n = 0; n <= 2; n++) {
+    for (let i = 0; i < ancientsData[gameInformation.numInArr].firstStage[n]; i++) {
+      let max = Math.floor(allChosenCardsArr[n].length);
+      let index = Math.floor(Math.random() * max);
+      firstStageArr.push(allChosenCardsArr[n][index]);
+      allChosenCardsArr[n].splice(index, 1);
+    }
   }
 
-}
+  for (let n = 0; n <= 2; n++) {
+    for (let i = 0; i < ancientsData[gameInformation.numInArr].secondStage[n]; i++) {
+      let max = Math.floor(allChosenCardsArr[n].length);
+      let index = Math.floor(Math.random() * max);
+      secondStageArr.push(allChosenCardsArr[n][index]);
+      allChosenCardsArr[n].splice(index, 1);
+    }
+  }
+
+  for (let n = 0; n <= 2; n++) {
+    for (let i = 0; i < ancientsData[gameInformation.numInArr].thirdStage[n]; i++) {
+      let max = Math.floor(allChosenCardsArr[n].length);
+      let index = Math.floor(Math.random() * max);
+      thirdStageArr.push(allChosenCardsArr[n][index]);
+      allChosenCardsArr[n].splice(index, 1);
+    }
+  }
+
+  allStageArr.push(shuffleArray(firstStageArr));
+  allStageArr.push(shuffleArray(secondStageArr));
+  allStageArr.push(shuffleArray(thirdStageArr));
+  
+  // flattenArr(allStageArr);
+
+  // allStageArrObj.arr = allStageArr;
+
+  allStageArrObj.arrFlatten = flattenArr(allStageArr);
+
+  return allStageArr;
+};
+
+/* Показ карт */
+
+// cardCount; глобальаня переменная со значением общего количества карт для древнего
+// allStageArrGlobal; глобальаня переменная с общим массивом карт
+
+const setBg = () => {
+  let allStageArrBgFlatten = allStageArrObj.arrFlatten;
+  console.log(allStageArrBgFlatten);
+
+  if (allStageArrBgFlatten.length > 0) {
+    cardPlace.style.backgroundImage = `url('${allStageArrBgFlatten[0]}')`;
+    allStageArrBgFlatten.shift(allStageArrBgFlatten[0]);
+  } else {
+    cardPlace.style.backgroundImage = `url('${allStageArrBgFlatten[0]}')`;
+    cardPlace.textContent = 'Вы перебрали все доступные карты!';
+  }
+};
+
 
 const chooseCards = () => {
   if (gameInformation.idGod === ancientsData[0].id) {
@@ -158,9 +260,7 @@ const chooseCards = () => {
         console.log(difficulties[1].id);
         break;
       case difficulties[2].id :
-        // console.log(difficulties[2].id);
-        getImgArr();
-        getCardsForFirstStage();
+        getCardsForAllStage();
         break;
       case difficulties[3].id :
         console.log(difficulties[3].id);
@@ -169,15 +269,62 @@ const chooseCards = () => {
         console.log(difficulties[4].id);
         break;
     }
-    // console.log(ancientsData[0].allCardsCount)
   } else if (gameInformation.idGod === ancientsData[1].id) {
-    console.log(ancientsData[0].allGreenCardsCount)
+    switch(gameInformation.idDifficulty) {
+      case difficulties[0].id :
+        console.log(difficulties[0].id);
+        break;
+      case difficulties[1].id :
+        console.log(difficulties[1].id);
+        break;
+      case difficulties[2].id :
+        getCardsForAllStage();
+        break;
+      case difficulties[3].id :
+        console.log(difficulties[3].id);
+        break;
+      case difficulties[4].id :
+        console.log(difficulties[4].id);
+        break;
+    }
   } else if (gameInformation.idGod === ancientsData[2].id) {
-    console.log(ancientsData[0].allBrownCardsCount)
+    switch(gameInformation.idDifficulty) {
+      case difficulties[0].id :
+        console.log(difficulties[0].id);
+        break;
+      case difficulties[1].id :
+        console.log(difficulties[1].id);
+        break;
+      case difficulties[2].id :
+        getCardsForAllStage();
+        break;
+      case difficulties[3].id :
+        console.log(difficulties[3].id);
+        break;
+      case difficulties[4].id :
+        console.log(difficulties[4].id);
+        break;
+    }
   } else if (gameInformation.idGod === ancientsData[3].id) {
-    console.log(ancientsData[0].allBlueCardsCount)
+    switch(gameInformation.idDifficulty) {
+      case difficulties[0].id :
+        console.log(difficulties[0].id);
+        break;
+      case difficulties[1].id :
+        console.log(difficulties[1].id);
+        break;
+      case difficulties[2].id :
+        getCardsForAllStage();
+        break;
+      case difficulties[3].id :
+        console.log(difficulties[3].id);
+        break;
+      case difficulties[4].id :
+        console.log(difficulties[4].id);
+        break;
+    }
   }
-}
+};
 
 const visiableDeckContainer = () => {
   deckContainer.classList.remove('hidden');
@@ -186,14 +333,44 @@ const visiableDeckContainer = () => {
 };
 
 deckButton.addEventListener('click', visiableDeckContainer);
+deskBackground.addEventListener('click', setBg);
 
 
 
 
 
 
+// function shuffleArray(arr) {
+//   let res = [];
+//   let len = arr.length;
+//   while (len > 0) {
+//     let ndx = Math.floor(Math.random() * len);
+//     res.push(arr[ndx]);
+//     arr.splice(ndx, 1);
+//     len--;
+//   }
+//   return res;
+// }
 
+  // console.log(allStageArrBg);
+  // if (allStageArrBg[0].length > 0) {
+  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[0][0]}')`;
+  //   // allStageArrBg[0].shift(allStageArrBg[0][0]);
+  //   allStageArrBg[0].splice(0, 1);
+  // }
 
+  // if (allStageArrBg[0].length === 0 && allStageArrBg[1].length > 0) {
+  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[1][0]}')`;
+  //   // allStageArrBg[1].shift(allStageArrBg[1][0]);
+  //   allStageArrBg[1].splice(0, 1);
+  // } 
+
+  // if (allStageArrBg[0].length === 0 && allStageArrBg[1].length === 0) {
+  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[2][0]}')`;
+  //   // allStageArrBg[2].shift(allStageArrBg[2][0]);
+  //   allStageArrBg[2].splice(0, 1);
+  // }
+  // console.log(allStageArrBg);
 
 
 
