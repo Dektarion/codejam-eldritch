df5bdc1fcafe20ce107884e3b38b6ada64a7dba9
Dektarion <andreiy.boyarsky@gmail.com>
2022-08-24 23:43:45 +0300

    nothing interesting

--- scripts/main.js
+++ scripts/main.js
@@ -4,18 +4,13 @@ import blueCardsData from '../data/mythicCards/blue/index.js';
 import brownCardsData from '../data/mythicCards/brown/index.js';
 import greenCardsData from '../data/mythicCards/green/index.js';
 
-// console.log(greenCardsData[0].cardFace);
-// console.log(ancientsData[0].allCardsCount);
-// document.querySelector('.myImg').src = 'https://placekitten.com/500/500' // or ./assets/platform.png
-// document.querySelector('.cthulhu').style.backgroundImage = `url('${ancientsData[0].cardFace}')`;
-
 const ancientsContainer = document.querySelector('.ancients__display');
 const difficultyButtonsContainer = document.querySelector('.difficulty__display');
 const ancients = document.querySelectorAll('.ancients');
 const difficultyButtons = document.querySelectorAll('.difficulty__buttons');
 const deckButtonDisplay = document.querySelector('.deck-button__display');
 const deckContainer = document.querySelector('.deck__container');
-const deckButton = document.querySelector('.deck-button');
+const shuffleButton = document.querySelector('.shuffle-button');
 const cardPlace = document.querySelector('.card');
 const deskBackground = document.querySelector('.deck');
 
@@ -24,28 +19,27 @@ const deskBackground = document.querySelector('.deck');
 let gameInformation = {
   idGod: '',
   idDifficulty: '',
-  numInArr: 0,
+  numGodInArr: null,
 };
 
-let allStageArrObj = {};
-
-
-const addNumGod = () => {
-  switch(gameInformation.idGod) {
+const addNumGod = (idOfGod) => {
+  switch(idOfGod) {
     case ancientsData[0].id :
-      gameInformation.numInArr = 0;
+      gameInformation.numGodInArr = 0;
       break;
     case ancientsData[1].id :
-      gameInformation.numInArr = 1;
+      gameInformation.numGodInArr = 1;
       break;
     case ancientsData[2].id :
-      gameInformation.numInArr = 2;
+      gameInformation.numGodInArr = 2;
       break;
     case ancientsData[3].id :
-      gameInformation.numInArr = 3;
+      gameInformation.numGodInArr = 3;
       break;
   }
 };
+
+const min = 0;
 
 /* Выбор древнего */
 
@@ -55,11 +49,10 @@ const activeAncient = (event) => {
   }
   if (event.target.classList.contains('ancients')) {
     gameInformation.idGod = event.target.id;
-    addNumGod();
+    addNumGod(gameInformation.idGod);
     event.target.classList.add('active_ancient');
     difficultyButtonsContainer.classList.remove('difficulty__display_hidden');
     deckContainer.classList.add('hidden');
-    
   }
 };
 
@@ -74,68 +67,78 @@ const activeDifficultyButtons = (event) => {
     event.target.classList.add('difficulty__buttons_active');
     deckButtonDisplay.classList.remove('deck-button__display_hidden');
     deckContainer.classList.add('hidden');
+    console.log(gameInformation);
   }
 };
 
 ancientsContainer.addEventListener('click', activeAncient);
 difficultyButtonsContainer.addEventListener('click', activeDifficultyButtons);
 
+/* Показ колоды и трекера карт */
+
+const visiableDeckContainer = () => {
+  deckContainer.classList.remove('hidden');
+  deckButtonDisplay.classList.add('deck-button__display_hidden');
+};
+
+shuffleButton.addEventListener('click', visiableDeckContainer);
+
 /* Выбор нужного количества карт каждого цвета */
 
 const getGreenCardsImgArr = () => {
-  const min = 0;
   const max = 17;
   let randomGreenImgArr = [];
   let check;
 
-  for (let i = 0; i < ancientsData[gameInformation.numInArr].allGreenCardsCount; i++) {
+  for (let i = 0; i < ancientsData[gameInformation.numGodInArr].allGreenCardsCount; i++) {
     do {
       let num =  Math.floor(Math.random() * (max - min + 1)) + min;
-      check = randomGreenImgArr.includes(greenCardsData[num].cardFace);
+      check = randomGreenImgArr.includes(greenCardsData[num]);
       if(!check){
-        randomGreenImgArr.push(greenCardsData[num].cardFace);      
+        randomGreenImgArr.push(greenCardsData[num]);      
       }
     }
     while(check);
   }
+  
   return randomGreenImgArr;
 };
 
 const getBrownCardsImgArr = () => {
-  const min = 0;
   const max = 20;
   let randomBrownImgArr = [];
   let check;
 
-  for (let i = 0; i < ancientsData[gameInformation.numInArr].allBrownCardsCount; i++) {
+  for (let i = 0; i < ancientsData[gameInformation.numGodInArr].allBrownCardsCount; i++) {
     do {
       let num =  Math.floor(Math.random() * (max - min + 1)) + min;
-      check = randomBrownImgArr.includes(brownCardsData[num].cardFace);
+      check = randomBrownImgArr.includes(brownCardsData[num]);
       if(!check){
-        randomBrownImgArr.push(brownCardsData[num].cardFace);      
+        randomBrownImgArr.push(brownCardsData[num]);      
       }
     }
     while(check);
   }
+  
   return randomBrownImgArr;
 };
 
 const getBlueCardsImgArr = () => {
-  const min = 0;
   const max = 11;
   let randomBlueImgArr = [];
   let check;
 
-  for (let i = 0; i < ancientsData[gameInformation.numInArr].allBlueCardsCount; i++) {
+  for (let i = 0; i < ancientsData[gameInformation.numGodInArr].allBlueCardsCount; i++) {
     do {
       let num =  Math.floor(Math.random() * (max - min + 1)) + min;
-      check = randomBlueImgArr.includes(blueCardsData[num].cardFace);
+      check = randomBlueImgArr.includes(blueCardsData[num]);
       if(!check){
-        randomBlueImgArr.push(blueCardsData[num].cardFace);      
+        randomBlueImgArr.push(blueCardsData[num]);      
       }
     }
     while(check);
   }
+
   return randomBlueImgArr;
 };
 
@@ -143,10 +146,13 @@ const getBlueCardsImgArr = () => {
 
 const getImgArr = () => {
   let allChosenCardsArr = [];
+  let greenCards = getGreenCardsImgArr();
+  let brownCards = getBrownCardsImgArr();
+  let blueCards = getBlueCardsImgArr();
 
-  allChosenCardsArr.push(getGreenCardsImgArr());
-  allChosenCardsArr.push(getBrownCardsImgArr());
-  allChosenCardsArr.push(getBlueCardsImgArr());
+  allChosenCardsArr.push(greenCards);
+  allChosenCardsArr.push(brownCards);
+  allChosenCardsArr.push(blueCards);
 
   return allChosenCardsArr;
 };
@@ -183,6 +189,9 @@ const flattenArr = function(arr) {
   return arrAllValues;
 };
 
+const allStageArrObj = {
+  arrFlatten: [],
+};
 
 const getCardsForAllStage = () => {
   let allStageArr = [];
@@ -192,7 +201,7 @@ const getCardsForAllStage = () => {
   let allChosenCardsArr = getImgArr();
 
   for (let n = 0; n <= 2; n++) {
-    for (let i = 0; i < ancientsData[gameInformation.numInArr].firstStage[n]; i++) {
+    for (let i = 0; i < ancientsData[gameInformation.numGodInArr].firstStage[n]; i++) {
       let max = Math.floor(allChosenCardsArr[n].length);
       let index = Math.floor(Math.random() * max);
       firstStageArr.push(allChosenCardsArr[n][index]);
@@ -201,7 +210,7 @@ const getCardsForAllStage = () => {
   }
 
   for (let n = 0; n <= 2; n++) {
-    for (let i = 0; i < ancientsData[gameInformation.numInArr].secondStage[n]; i++) {
+    for (let i = 0; i < ancientsData[gameInformation.numGodInArr].secondStage[n]; i++) {
       let max = Math.floor(allChosenCardsArr[n].length);
       let index = Math.floor(Math.random() * max);
       secondStageArr.push(allChosenCardsArr[n][index]);
@@ -210,7 +219,7 @@ const getCardsForAllStage = () => {
   }
 
   for (let n = 0; n <= 2; n++) {
-    for (let i = 0; i < ancientsData[gameInformation.numInArr].thirdStage[n]; i++) {
+    for (let i = 0; i < ancientsData[gameInformation.numGodInArr].thirdStage[n]; i++) {
       let max = Math.floor(allChosenCardsArr[n].length);
       let index = Math.floor(Math.random() * max);
       thirdStageArr.push(allChosenCardsArr[n][index]);
@@ -222,180 +231,69 @@ const getCardsForAllStage = () => {
   allStageArr.push(shuffleArray(secondStageArr));
   allStageArr.push(shuffleArray(thirdStageArr));
   
-  // flattenArr(allStageArr);
-
-  // allStageArrObj.arr = allStageArr;
+  console.log(allStageArr);
 
   allStageArrObj.arrFlatten = flattenArr(allStageArr);
 
-  return allStageArr;
+  // return flattenArr(allStageArr);
+  return allStageArrObj;
 };
+
+// allStageArrObj.arrFlatten = getCardsForAllStage();
+
+const chooseCards = () => {
+  switch(gameInformation.idDifficulty) {
+    case difficulties[0].id :
+
+      break;
+    case difficulties[1].id :
+
+      break;
+    case difficulties[2].id :
+      getCardsForAllStage();
+      break;
+    case difficulties[3].id :
+
+      break;
+    case difficulties[4].id :
+
+      break;
+  }
+};
+
+shuffleButton.addEventListener('click', () => {
+  allStageArrObj.arrFlatten = [];
+  chooseCards();
+  });
 
 /* Показ карт */
 
-// cardCount; глобальаня переменная со значением общего количества карт для древнего
-// allStageArrGlobal; глобальаня переменная с общим массивом карт
-
 const setBg = () => {
-  let allStageArrBgFlatten = allStageArrObj.arrFlatten;
-  console.log(allStageArrBgFlatten);
+  console.log(allStageArrObj.arrFlatten);
 
-  if (allStageArrBgFlatten.length > 0) {
-    cardPlace.style.backgroundImage = `url('${allStageArrBgFlatten[0]}')`;
-    allStageArrBgFlatten.shift(allStageArrBgFlatten[0]);
+  if (allStageArrObj.arrFlatten.length > 0) {
+    cardPlace.style.backgroundImage = `url('${allStageArrObj.arrFlatten[0].cardFace}')`;
+    allStageArrObj.arrFlatten.shift(allStageArrObj.arrFlatten[0]);
   } else {
-    cardPlace.style.backgroundImage = `url('${allStageArrBgFlatten[0]}')`;
-    cardPlace.textContent = 'Вы перебрали все доступные карты!';
+    cardPlace.style.backgroundImage = `url('null')`;
   }
 };
 
-
-const chooseCards = () => {
-  if (gameInformation.idGod === ancientsData[0].id) {
-    switch(gameInformation.idDifficulty) {
-      case difficulties[0].id :
-        console.log(difficulties[0].id);
-        break;
-      case difficulties[1].id :
-        console.log(difficulties[1].id);
-        break;
-      case difficulties[2].id :
-        getCardsForAllStage();
-        break;
-      case difficulties[3].id :
-        console.log(difficulties[3].id);
-        break;
-      case difficulties[4].id :
-        console.log(difficulties[4].id);
-        break;
-    }
-  } else if (gameInformation.idGod === ancientsData[1].id) {
-    switch(gameInformation.idDifficulty) {
-      case difficulties[0].id :
-        console.log(difficulties[0].id);
-        break;
-      case difficulties[1].id :
-        console.log(difficulties[1].id);
-        break;
-      case difficulties[2].id :
-        getCardsForAllStage();
-        break;
-      case difficulties[3].id :
-        console.log(difficulties[3].id);
-        break;
-      case difficulties[4].id :
-        console.log(difficulties[4].id);
-        break;
-    }
-  } else if (gameInformation.idGod === ancientsData[2].id) {
-    switch(gameInformation.idDifficulty) {
-      case difficulties[0].id :
-        console.log(difficulties[0].id);
-        break;
-      case difficulties[1].id :
-        console.log(difficulties[1].id);
-        break;
-      case difficulties[2].id :
-        getCardsForAllStage();
-        break;
-      case difficulties[3].id :
-        console.log(difficulties[3].id);
-        break;
-      case difficulties[4].id :
-        console.log(difficulties[4].id);
-        break;
-    }
-  } else if (gameInformation.idGod === ancientsData[3].id) {
-    switch(gameInformation.idDifficulty) {
-      case difficulties[0].id :
-        console.log(difficulties[0].id);
-        break;
-      case difficulties[1].id :
-        console.log(difficulties[1].id);
-        break;
-      case difficulties[2].id :
-        getCardsForAllStage();
-        break;
-      case difficulties[3].id :
-        console.log(difficulties[3].id);
-        break;
-      case difficulties[4].id :
-        console.log(difficulties[4].id);
-        break;
-    }
-  }
-};
-
-const visiableDeckContainer = () => {
-  deckContainer.classList.remove('hidden');
-  deckButtonDisplay.classList.add('deck-button__display_hidden');
-  chooseCards();
-};
-
-deckButton.addEventListener('click', visiableDeckContainer);
 deskBackground.addEventListener('click', setBg);
 
 
-
-
-
-
-// function shuffleArray(arr) {
-//   let res = [];
-//   let len = arr.length;
-//   while (len > 0) {
-//     let ndx = Math.floor(Math.random() * len);
-//     res.push(arr[ndx]);
-//     arr.splice(ndx, 1);
-//     len--;
-//   }
-//   return res;
+// const state = {
+//   deck: []
 // }
+// state.deck = shuffleDeck();
 
-  // console.log(allStageArrBg);
-  // if (allStageArrBg[0].length > 0) {
-  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[0][0]}')`;
-  //   // allStageArrBg[0].shift(allStageArrBg[0][0]);
-  //   allStageArrBg[0].splice(0, 1);
-  // }
+// zameshat'Button.addEventlistener('click', () => {
+//   state.deck = [];
+//   TVOY CODE...;
+// })
 
-  // if (allStageArrBg[0].length === 0 && allStageArrBg[1].length > 0) {
-  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[1][0]}')`;
-  //   // allStageArrBg[1].shift(allStageArrBg[1][0]);
-  //   allStageArrBg[1].splice(0, 1);
-  // } 
+/*просто на клик по сложности задать массив = []. тогда в следующую игру не пойдут
+ недоигранные карты. Мож, у вас их много, и вы просто запутались в них, и не все обнуляете? 
+ у меня их несколько, я все обнуляю */
 
-  // if (allStageArrBg[0].length === 0 && allStageArrBg[1].length === 0) {
-  //   cardPlace.style.backgroundImage = `url('${allStageArrBg[2][0]}')`;
-  //   // allStageArrBg[2].shift(allStageArrBg[2][0]);
-  //   allStageArrBg[2].splice(0, 1);
-  // }
-  // console.log(allStageArrBg);
-
-
-
-
-  // for (let i = iNum; i < ancientsData[gameInformation.numInArr].allGreenCardsCount; i++) {
-  //   iNum = i;
-  //   let num =  Math.floor(Math.random() * (max - min + 1)) + min;
-  //   if (randomNumArr.length === ancientsData[gameInformation.numInArr].allGreenCardsCount) {
-  //     return;
-  //   } else if (randomNumArr.includes(num)) {
-  //     getGreenCardsImgArr();
-  //   } else if (randomNumArr.length <= ancientsData[gameInformation.numInArr].allGreenCardsCount) {
-  //     // randomNumArr.push(greenCardsData[num].cardFace);
-  //     randomNumArr.push(num);
-  //   } 
-  // }
-  // console.log(randomNumArr);
-  // return randomNumArr;
-
-  // const getGreenCardsImgArr = () => {
-//   let allGreenCardsImgArr = [];
-//   for (let i = 0; i <= 17; i++) {
-//     allGreenCardsImgArr.push(greenCardsData[i].cardFace);
-//   }
-
-//   console.log(allGreenCardsImgArr);
-//   return allGreenCardsImgArr;
-// };
+/* повесить обнуление на кнопку генерации колоды */
